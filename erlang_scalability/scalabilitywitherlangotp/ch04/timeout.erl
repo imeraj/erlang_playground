9946d254cea443c6ae5efde36055ff0a0c627560
Meraj Molla <meraj.enigma@gmail.com>
2024-04-18 11:45:14 -0400

    rest gen_servers: ping and timeout

---
+++ erlang_scalability/scalabilitywitherlangotp/ch04/timeout.erl
@@ -0,0 +1,10 @@
+-module(timeout).
+-behaviour(gen_server).
+-export([init/1, handle_call/3]).
+
+init(_Args) ->
+  {ok, undefined}.
+
+handle_call({sleep, Ms}, _From, LoopData) ->
+  timer:sleep(Ms),
+  {reply, ok, LoopData}.
