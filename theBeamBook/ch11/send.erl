755d3660f64b6631e0db94d790ab6d04f64d5b09
Meraj Molla <meraj.enigma@gmail.com>
2025-07-28 11:11:14 -0400

    message passing

---
+++ theBeamBook/ch11/send.erl
@@ -0,0 +1,19 @@
+-module(send).
+
+-export([test/0]).
+
+test() ->
+    P2 = spawn(fun() -> p2() end),
+    P1 = spawn(fun() -> p1(P2) end),
+    {P1, P2}.
+
+p2 () ->
+    receive
+        M -> io:format("P2 got ~p~n", [M])
+    end.
+
+p1(P2) ->
+    L = "hello",
+    M = {L, L},
+    P2 ! M,
+    io:format("P1 sent ~p~n", [M]).
