927afa3f586efffedcb94f2235886cf7e663ab94
Meraj Molla <meraj.enigma@gmail.com>
2025-08-07 11:25:42 -0400

    user api - add, getall, get

--- web_with_nova/personaldb/src/controllers/personaldb_main_controller.erl
+++ web_with_nova/personaldb/src/controllers/personaldb_main_controller.erl
@@ -1,7 +1,53 @@
 -module(personaldb_main_controller).
 -export([
-         index/1
+         add/1,
+         index/1,
+         get_by_id/1
         ]).
 
+add(#{json := #{<<"age">> := Age,<<"id">> := Id}}) ->
+    try
+        {ok, Port} = eredis:start_link(),
+        {ok, Results} = eredis:q(Port, ["hset", "users" | [Id, Age]]),
+        eredis:stop(Port),
+        {json, 201, #{}, #{<<"resp">> => Results}}
+    catch
+        Error:Cause ->
+            {json,500,#{<<"Content-Type">> => <<"json">>},
+            #{<<"error">> => Error, <<"cause">> => Cause}}
+    end.
+
 index(_Req) ->
-    {ok, [{message, "Hello world!"}]}.
+    try
+        {ok, Port} = eredis:start_link(),
+        {ok, Results} = eredis:q(Port, ["hgetall", "users"]),
+        Resp = flat_list_to_map(Results, []),
+        eredis:stop(Port),
+        {json, 200, #{}, #{<<"resp">> => Resp}}
+   catch
+       Error:Cause ->
+           {json,500,#{<<"Content-Type">> => <<"json">>},
+           #{<<"error">> => Error, <<"cause">> => Cause}}
+   end.
+
+ get_by_id(#{parsed_qs := #{<<"id">> := UserId}}) ->
+       try
+           {ok, Port} = eredis:start_link(),
+           case eredis:q(Port, ["hget", "users", UserId]) of
+               {ok, undefined} ->
+                   eredis:stop(Port),
+                   {status, 404};
+               {ok, Results} ->
+                   eredis:stop(Port),
+                   {json, 200, #{}, #{<<"id">> => UserId, <<"age">> => Results}}
+           end
+       catch
+          Error:Cause ->
+              {json, 500, #{<<"Content-Type">> => <<"json">>},
+              #{<<"error">> => Error, <<"cause">> => Cause}}
+       end.
+
+flat_list_to_map([], Acc) ->
+    Acc;
+flat_list_to_map([K, V | Rest], Acc) ->
+    flat_list_to_map(Rest, [#{<<"id">> => K,  <<"age">> => V} | Acc]).
--- web_with_nova/personaldb/src/personaldb_router.erl
+++ web_with_nova/personaldb/src/personaldb_router.erl
@@ -7,10 +7,12 @@
 
 %% The Environment-variable is defined in your sys.config in {nova, [{environment, Value}]}
 routes(_Environment) ->
-    [#{prefix => "",
+    [#{prefix => "/users",
       security => false,
       routes => [
-                 {"/", fun personaldb_main_controller:index/1 , #{methods => [get]}},
+                 {"/", fun personaldb_main_controller:add/1 , #{methods => [post]}},
+                 {"/getall", fun personaldb_main_controller:index/1 , #{methods => [get]}},
+                 {"/get", fun personaldb_main_controller:get_by_id/1, #{methods => [get]}},
                  {"/heartbeat", fun(_) -> {status, 200} end, #{methods => [get]}}
                 ]
       }].
