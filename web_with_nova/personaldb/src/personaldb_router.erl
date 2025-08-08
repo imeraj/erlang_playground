b25dd5be1fe82ffa3c493b7d7041b2dd0feb56a7
Meraj Molla <meraj.enigma@gmail.com>
2025-08-08 06:39:21 -0400

    fix route to be more rest compliant

--- web_with_nova/personaldb/src/personaldb_router.erl
+++ web_with_nova/personaldb/src/personaldb_router.erl
@@ -1,18 +1,14 @@
 -module(personaldb_router).
 -behaviour(nova_router).
 
--export([
-         routes/1
-        ]).
+-export([routes/1]).
 
 %% The Environment-variable is defined in your sys.config in {nova, [{environment, Value}]}
 routes(_Environment) ->
     [#{prefix => "/users",
-      security => false,
-      routes => [
-                 {"/", fun personaldb_main_controller:add/1 , #{methods => [post]}},
-                 {"/getall", fun personaldb_main_controller:index/1 , #{methods => [get]}},
-                 {"/get", fun personaldb_main_controller:get_by_id/1, #{methods => [get]}},
-                 {"/heartbeat", fun(_) -> {status, 200} end, #{methods => [get]}}
-                ]
-      }].
+       security => false,
+       routes =>
+           [{"/", fun personaldb_main_controller:add/1, #{methods => [post]}},
+            {"/", fun personaldb_main_controller:index/1, #{methods => [get]}},
+            {"/:id", fun personaldb_main_controller:get_by_id/1, #{methods => [get]}},
+            {"/heartbeat", fun(_) -> {status, 200} end, #{methods => [get]}}]}].
