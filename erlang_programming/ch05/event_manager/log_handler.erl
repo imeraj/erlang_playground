a9954c3e014ce48c15b6138985a91eb1584f4d48
Meraj Molla <meraj.enigma@gmail.com>
2025-10-23 09:57:44 -0400

    event handlers

---
+++ erlang_programming/ch05/event_manager/log_handler.erl
@@ -0,0 +1,16 @@
+-module(log_handler).
+-export([init/1, terminate/1, handle_event/2]).
+
+init(File) ->
+  {ok, Fd} = file:open(File, write),
+  Fd.
+
+terminate(Fd) ->
+  file:close(Fd).
+
+handle_event({Action, Id, Event}, Fd) ->
+  {MegaSec, Sec, MicroSec} = os:timestamp(),
+  io:format(Fd, "~w,~w,~w,~w,~w,~p~n", [MegaSec, Sec, MicroSec, Action, Id, Event]),
+  Fd;
+handle_event(_, Fd) ->
+  Fd.
